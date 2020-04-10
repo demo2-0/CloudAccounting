95573a8913697c4b6b33584e6db1aa9b1a86cf00
qq528468688 <528468688@qq.com>
2020-04-10 10:09:14 +0800

    1111

--- js/index.js
+++ js/index.js
@@ -1,161 +1,72 @@
+ 
+ layui.define(['jquery', 'element', 'carousel', 'laypage'], function(exports){
+   var $ = layui.jquery
+   ,element = layui.element
+   ,carousel = layui.carousel
+   ,laypage = layui.laypage;
+ 
+   //轮播渲染
+   carousel.render({
+     elem: '#banner'
+     ,width: '100%'
+     ,height: '898px'
+     ,arrow: 'always'
+   });
+ 
+   //轮播文字
+   $(function(){
+     $('.banner').children('.title').addClass('active');
+   })
+ 
+ 
+   exports('index', {}); 
+ });
 
-$(function () {
-    //加载弹出层
-    layui.use(['form','element'],
-    function() {
-        layer = layui.layer;
-        element = layui.element;
-    });
+layui.define(['layer', 'form'], function(exports){
+  
+  var $ = layui.jquery
+  ,layer = layui.layer
+  ,form = layui.form
+  ,device = layui.device()
 
-    //触发事件
-  var tab = {
-        tabAdd: function(title,url,id){
-          //新增一个Tab项
-          element.tabAdd('xbs_tab', {
-            title: title 
-            ,content: '<iframe tab-id="'+id+'" frameborder="0" src="'+url+'" scrolling="yes" class="x-iframe"></iframe>'
-            ,id: id
-          })
-        }
-        ,tabDelete: function(othis){
-          //删除指定Tab项
-          element.tabDelete('xbs_tab', '44'); //删除：“商品管理”
-          
-          
-          othis.addClass('layui-btn-disabled');
-        }
-        ,tabChange: function(id){
-          //切换到指定Tab项
-          element.tabChange('xbs_tab', id); //切换到：用户管理
-        }
-      };
+  ,DISABLED = 'layui-btn-disabled';
+  
+  //阻止IE7以下访问
+  if(device.ie && device.ie < 8){
+    layer.alert('如果您非得使用 IE 浏览器访问Fly社区，那么请使用 IE8+');
+  }
+   
+  //搜索
+  $('.c-search').on('click', function(){
+    layer.open({
+      type: 1
+      ,title: false
+      ,closeBtn: false
+      //,shade: [0.1, '#fff']
+      ,shadeClose: true
+      ,maxWidth: 10000
+      ,skin: 'c-layer-search'
+      ,content: ['<form action="http://cn.bing.com/search">'
+        ,'<input autocomplete="off" placeholder="搜索内容，回车跳转" type="text" name="q">'
+      ,'</form>'].join('')
+      ,success: function(layero){
+        var input = layero.find('input');
+        input.focus();
 
-
-    tableCheck = {
-        init:function  () {
-            $(".layui-form-checkbox").click(function(event) {
-                if($(this).hasClass('layui-form-checked')){
-                    $(this).removeClass('layui-form-checked');
-                    if($(this).hasClass('header')){
-                        $(".layui-form-checkbox").removeClass('layui-form-checked');
-                    }
-                }else{
-                    $(this).addClass('layui-form-checked');
-                    if($(this).hasClass('header')){
-                        $(".layui-form-checkbox").addClass('layui-form-checked');
-                    }
-                }
-                
-            });
-        },
-        getData:function  () {
-            var obj = $(".layui-form-checked").not('.header');
-            var arr=[];
-            obj.each(function(index, el) {
-                arr.push(obj.eq(index).attr('data-id'));
-            });
-            return arr;
-        }
-    }
-
-    //开启表格多选
-    tableCheck.init();
-      
-
-    $('.container .left_open i').click(function(event) {
-        if($('.left-nav').css('left')=='0px'){
-            $('.left-nav').animate({left: '-221px'}, 100);
-            $('.page-content').animate({left: '0px'}, 100);
-            $('.page-content-bg').hide();
-        }else{
-            $('.left-nav').animate({left: '0px'}, 100);
-            $('.page-content').animate({left: '221px'}, 100);
-            if($(window).width()<768){
-                $('.page-content-bg').show();
-            }
-        }
-
-    });
-
-    $('.page-content-bg').click(function(event) {
-        $('.left-nav').animate({left: '-221px'}, 100);
-        $('.page-content').animate({left: '0px'}, 100);
-        $(this).hide();
-    });
-
-    $('.layui-tab-close').click(function(event) {
-        $('.layui-tab-title li').eq(0).find('i').remove();
-    });
-
-   $("tbody.x-cate tr[fid!='0']").hide();
-    // 栏目多级显示效果
-    $('.x-show').click(function () {
-        if($(this).attr('status')=='true'){
-            $(this).html('&#xe625;'); 
-            $(this).attr('status','false');
-            cateId = $(this).parents('tr').attr('cate-id');
-            $("tbody tr[fid="+cateId+"]").show();
-       }else{
-            cateIds = [];
-            $(this).html('&#xe623;');
-            $(this).attr('status','true');
-            cateId = $(this).parents('tr').attr('cate-id');
-            getCateId(cateId);
-            for (var i in cateIds) {
-                $("tbody tr[cate-id="+cateIds[i]+"]").hide().find('.x-show').html('&#xe623;').attr('status','true');
-            }
-       }
+        layero.find('form').submit(function(){
+          var val = input.val();
+          if(val.replace(/\s/g, '') === ''){
+            return false;
+          }
+          input.val('site:layui.com '+ input.val());
+      });
+      }
     })
+  });
 
-    //左侧菜单效果
-    // $('#content').bind("click",function(event){
-	//点击菜单显示效果	
-	$(document).ready(function() {
-       $('.left-nav #nav li .sub-menu li ').click(function(){
-		   $(this).addClass('menu-current').siblings().removeClass('menu-current');
-		   })
-    });	
-	
-    $('.left-nav #nav li').click(function (event) {
+});
 
-        if($(this).children('.sub-menu').length){
-            if($(this).hasClass('open')){
-                $(this).removeClass('open');
-                $(this).find('.nav_right').html('&#xe6a7;');
-                $(this).children('.sub-menu').stop().slideUp();
-                $(this).siblings().children('.sub-menu').slideUp();
-				
-            }else{
-                $(this).addClass('open');
-                $(this).children('a').find('.nav_right').html('&#xe6a6;');
-                $(this).children('.sub-menu').stop().slideDown();
-                $(this).siblings().children('.sub-menu').stop().slideUp();
-                $(this).siblings().find('.nav_right').html('&#xe6a7;');
-                $(this).siblings().removeClass('open');
-            }
-        }else{
 
-            var url = $(this).children('a').attr('_href');
-            var title = $(this).find('cite').html();
-            var index  = $('.left-nav #nav li').index($(this));
-
-            for (var i = 0; i <$('.x-iframe').length; i++) {
-                if($('.x-iframe').eq(i).attr('tab-id')==index+1){
-                    tab.tabChange(index+1);
-                    event.stopPropagation();
-                    return;
-                }
-            };
-            
-            tab.tabAdd(title,url,index+1);
-            tab.tabChange(index+1);
-        }
-        
-        event.stopPropagation();
-         
-    })
-    
-})
 var cateIds = [];
 function getCateId(cateId) {
     
